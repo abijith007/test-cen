e592b2485230a9b1fba3636454705969c2c5aa8c
Abijith <abijithtrichurr.cs17@rvce.edu.in>
2021-04-21 19:13:21 +0530

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,28 @@
 FROM node:10-alpine as builder
 
+EXPOSE 600
+
+RUN mkdir /cookie-test-folder
+
+WORKDIR /cookie-test-folder
+
 COPY package.json package-lock.json ./
 
 RUN npm install 
 
+
 COPY . .
 
-RUN node index.js
 
 # Stage 2 - Deploy with NGNIX
 FROM nginx:1.16.0-alpine
 
-COPY --from=builder /index.html /usr/share/nginx/html
+COPY . .
+
+COPY --from=builder /cookie-test-folder/index.html /usr/share/nginx/html
 RUN rm /etc/nginx/conf.d/default.conf
 COPY nginx/nginx.conf /etc/nginx/conf.d
-EXPOSE 80
+
+EXPOSE 500
+
 CMD ["nginx", "-g", "daemon off;"]
--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const port = 600;
 
 var axios = require("axios");
 
+function getIndicesOf(searchStr, str, caseSensitive) {
+  var searchStrLen = searchStr.length;
+  if (searchStrLen == 0) {
+    return [];
+  }
+  var startIndex = 0,
+    index,
+    indices = [];
+  if (!caseSensitive) {
+    str = str.toLowerCase();
+    searchStr = searchStr.toLowerCase();
+  }
+  while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+    indices.push(index);
+    startIndex = index + searchStrLen;
+  }
+  return indices;
+}
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -16,33 +35,37 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
   var config = {
     method: "get",
-    url: "https://rvcemeet1.cen.org.in/b/",
+    url: "https://rvcemeet1.cen.org.in/b/signin",
   };
 
   axios(config)
     .then(function (response) {
       cookie_data = response.headers["set-cookie"];
-      config.url = "https://rvcemeet1.cen.org.in/b/signin";
-      axios(config)
-        .then((response) => {
-          res_text = JSON.stringify(response.data);
-          index = res_text.search("csrf-token") + 22;
-          csrf_data = "";
-          for (i = 0; i < 100; i++) {
-            csrf_data += res_text[index + i];
-          }
-          pattern = /[A-Z,a-z,0-9,\=,\\,\/,\+]*==/g;
-          csrf_token = csrf_data.match(pattern);
-          csrf_token = csrf_token[0];
-          cookie_data = cookie_data[0];
-          body = { cookie_data, csrf_token };
-          console.log(body);
-          res.send(body);
-        })
-        .catch((error) => {
-          console.log(error);
-          res.send(error);
-        });
+
+      res_text = JSON.stringify(response.data);
+      index = res_text.search("csrf-token") + 20;
+      csrf_data = "";
+      for (i = 0; i < 120; i++) {
+        csrf_data += res_text[index + i];
+      }
+      //console.log(csrf_data);
+
+      values = getIndicesOf("authenticity_token", res_text);
+      authenticity_token = "";
+      for (i = values[1] + 25; i < 130 + values[1]; i++) {
+        authenticity_token += res_text[i];
+      }
+      console.log(authenticity_token);
+      pattern = /[A-Z,a-z,0-9,\=,\\,\/,\+]*==/g;
+
+      authenticity_token = authenticity_token.match(pattern);
+      console.log(authenticity_token);
+      csrf_token = csrf_data.match(pattern);
+      csrf_token = csrf_token[0];
+      cookie_data = cookie_data[0];
+      body = { cookie_data, csrf_token, authenticity_token };
+      console.log(body);
+      res.send(body);
     })
     .catch(function (error) {
       console.log(error);
